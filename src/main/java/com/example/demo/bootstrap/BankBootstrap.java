cf861bddc2a0c1772a392b87144de17d6700d9c9
Shabdan <shabdan.abjaparov.official@gmail.com>
2019-05-25 20:09:45 +0600

    trying to create tests

--- src/main/java/com/example/demo/bootstrap/BankBootstrap.java
+++ src/main/java/com/example/demo/bootstrap/BankBootstrap.java
@@ -1,14 +1,7 @@
 package com.example.demo.bootstrap;
 
-import com.example.demo.model.Bank;
 import com.example.demo.model.Client;
 import com.example.demo.model.Credit;
-import com.example.demo.model.Payment;
-import com.example.demo.repository.BankRepository;
-import com.example.demo.repository.ClientRepository;
-import com.example.demo.repository.CreditRepository;
-import com.example.demo.repository.PaymentRepository;
-import com.example.demo.service.BankService;
 import com.example.demo.service.ClientService;
 import com.example.demo.service.CreditService;
 import com.example.demo.service.PaymentService;
@@ -17,9 +10,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.sql.Time;
-import java.time.LocalDateTime;
-
 @Component
 public class BankBootstrap implements CommandLineRunner {
     @Autowired
@@ -31,23 +21,19 @@ public class BankBootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception{
-//        Bank bank1 = new Bank("BakaiBank");
-//        bankService.createBank(bank1);
-//        Client client1 = new Client("Medet uulu Shabdan", "+996-(550)-523-209");
-//        Client client2 = new Client("Kasymov Temirlan","+996-(770)-443-458");
-//        Client client3 = new Client("Pamirbek Almazov","+996-(700)-555-243");
-//        clientService.createClient(client1);
-//        clientService.createClient(client2);
-//        clientService.createClient(client3);
-//        Credit credit1 = new Credit(client1,"MicroCompanyCredit","USD",BigDecimal.valueOf(12000.0),12,15.0,null);
-//        Credit credit2 = new Credit(client1,"BussinessProperty","USD",BigDecimal.valueOf(9000.0),8,10.0,null);
-//        Credit credit3 = new Credit(client2,"Estate","EUR",BigDecimal.valueOf(1300.0),9,5.0,null);
-//        Credit credit4 = new Credit(client3,"Ipoteka","SOM",BigDecimal.valueOf(12000.0),4,20.0,null);
-//        creditService.createCredit(credit1);
-//        creditService.createCredit(credit2);
-//        creditService.createCredit(credit3);
-//        creditService.createCredit(credit4);
-//        Payment payment1 = new Payment("new payment", BigDecimal.valueOf(250), LocalDateTime.now(),"1267476Sha",credit1);
-//        paymentService.makePayment(payment1);
+        Client client1 = new Client("Medet uulu Shabdan", "+996-(550)-523-209");
+        Client client2 = new Client("Kasymov Temirlan","+996-(770)-443-458");
+        Client client3 = new Client("Pamirbek Almazov","+996-(700)-555-243");
+        clientService.createClient(client1);
+        clientService.createClient(client2);
+        clientService.createClient(client3);
+        Credit credit1 = new Credit(client1,"MicroCompanyCredit","USD",BigDecimal.valueOf(12000.0),12,15.0,null);
+        Credit credit2 = new Credit(client1,"BussinessProperty","USD",BigDecimal.valueOf(9000.0),8,10.0,null);
+        Credit credit3 = new Credit(client2,"Estate","EUR",BigDecimal.valueOf(1300.0),9,5.0,null);
+        Credit credit4 = new Credit(client3,"Ipoteka","SOM",BigDecimal.valueOf(12000.0),4,20.0,null);
+        creditService.createCredit(credit1);
+        creditService.createCredit(credit2);
+        creditService.createCredit(credit3);
+        creditService.createCredit(credit4);
     }
 }
--- src/main/java/com/example/demo/repository/BankRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface BankRepository extends JpaRepository<Bank,Long> {
-}
